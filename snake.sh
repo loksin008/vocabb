52b38c3abcbfcac4a1a0d29987c2fc776ddea838
loksin008 <79530873+loksin008@users.noreply.github.com>
2024-11-06 13:35:21 +0530

    Update snake.sh

--- snake.sh
+++ snake.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
-# Initialize the screen size
-width=50
-height=20
+# Get the terminal screen size dynamically
+width=$(tput cols)
+height=$(tput lines)
 
-# Snake body represented by characters
-snake="\e[31mO\e[0m"  # Red color for the snake
-tail="\e[31mo\e[0m"   # Red color for the tail (if you want to use it later)
+# Snake representation in red color
+head="\e[31mO\e[0m"   # Snake head
+body="\e[31mo\e[0m"   # Snake body
 
-# Snake's initial position
-x=10
-y=10
+# Snake's initial position, length, and direction
+x=$((width / 2))
+y=$((height / 2))
+length=20  # Initial length of the snake
 
-# Velocity
+# Arrays to track the snake's body positions
+snake_x=($x)
+snake_y=($y)
+
+# Initial velocity (moving right)
 dx=1
 dy=0
 
@@ -26,30 +31,57 @@ do
   clear
   tput cup 0 0
   
-  # Draw the snake's position on the screen
+  # Draw the snake on the screen
   for ((i=0; i<$height; i++))
   do
     for ((j=0; j<$width; j++))
     do
-      if [[ $i -eq $y && $j -eq $x ]]; then
-        echo -ne "$snake"
-      else
+      drawn=false
+      # Draw the snake body
+      for ((k=0; k<${#snake_x[@]}; k++))
+      do
+        if [[ $i -eq ${snake_y[$k]} && $j -eq ${snake_x[$k]} ]]; then
+          if [[ $k -eq 0 ]]; then
+            echo -ne "$head"  # Head of the snake
+          else
+            echo -ne "$body"  # Body of the snake
+          fi
+          drawn=true
+          break
+        fi
+      done
+      if [ "$drawn" = false ]; then
         echo -n " "
       fi
     done
     echo
   done
   
-  # Move the snake
+  # Move the snake by updating its position arrays
   x=$((x + dx))
   y=$((y + dy))
+  
+  # Insert new head position at the beginning of the arrays
+  snake_x=($x "${snake_x[@]:0:$((length-1))}")
+  snake_y=($y "${snake_y[@]:0:$((length-1))}")
 
-  # Boundary check and snake bouncing
+  # Bounce the snake off the walls
   if [[ $x -ge $width || $x -lt 0 ]]; then
     dx=$((dx * -1))
   fi
   if [[ $y -ge $height || $y -lt 0 ]]; then
     dy=$((dy * -1))
+  fi
+
+  # Change direction randomly every few steps
+  if (( RANDOM % 10 == 0 )); then
+    direction=$((RANDOM % 4))
+    case $direction in
+      0) dx=1; dy=0 ;;   # Move right
+      1) dx=-1; dy=0 ;;  # Move left
+      2) dx=0; dy=1 ;;   # Move down
+      3) dx=0; dy=-1 ;;  # Move up
+    esac
   fi
 
   # Slow down the animation
